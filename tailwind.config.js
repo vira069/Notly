e26f833e119068a32d5bc1394ba7bc5c97e15e4e
vira069 <vira.kumar069@gmail.com>
2024-12-16 01:27:16 +0530

    The Note saver react app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
